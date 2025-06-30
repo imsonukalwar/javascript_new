a8a43754c07fb3304aaf2b5f5f61fcaf53ce9dd6
unknown <imsonukalwar@gmail.com>
2025-06-30 22:56:00 +0530

    add

---
+++ function/03function.js
@@ -0,0 +1,11 @@
+function one(){
+    const user="sonu"
+    function two(){
+        const website="youtube"
+        console.log(user);
+    }
+   // console.log(website);
+    two();
+    
+}
+one()
