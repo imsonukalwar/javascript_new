999060db54206167db4e1c6c4f8aae4b95f7377f
unknown <imsonukalwar@gmail.com>
2025-07-01 20:34:29 +0530

    change

--- function/03function.js
+++ function/03function.js
@@ -5,7 +5,34 @@ function one(){
         console.log(user);
     }
    // console.log(website);
-    two();
+   // two();
     
 }
-one()
+// one()
+
+
+if (true) {
+    const username="sonu "
+    if(username==="sonu"){
+        const website ="youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+
+// +++++++++++++++++++>>>>>>>>>>>>>>>>>>>>>>>
+
+function addone(num){
+    return num+1
+}
+console.log(addone(4));
+
+
+const add=function addtwo(num){
+    return num+2
+}
+console.log(add(4));
