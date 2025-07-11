6efec03bf84cc5dbfa75bd663ebf7d48f0076b24
unknown <imsonukalwar@gmail.com>
2025-07-11 19:36:58 +0530

    new file

---
+++ control_flow/02two.js
@@ -0,0 +1,32 @@
+// const score =200;
+// if(score>100){
+//     const power="fly";
+//     console.log(`user power ${power}`);
+// }
+//    console.log(`user power ${power}`);
+
+const balance =1000
+// if(balance>500)console.log("test"),console.log("test2");
+
+if(balance<500){
+    console.log("less than 500");
+}
+else if(balance<900){
+    console.log("less than 1000");
+}
+else if(balance==1000){
+    console.log("balance is equal to 1000");
+    
+}
+
+const userloged=true;
+const debitcard=true;
+const logfromgoogle=false;
+const logfromemail=true;
+if(userloged && debitcard &&2==3){
+    console.log("allow to buy a couse");
+}
+if(userloged || logfromemail){
+    console.log("user log in");
+    
+}
