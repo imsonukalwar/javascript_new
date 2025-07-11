a059a37c2166d311d7a734fe40840f565aacff6a
unknown <imsonukalwar@gmail.com>
2025-07-11 19:56:17 +0530

    new file

---
+++ control_flow/swich_case.js
@@ -0,0 +1,50 @@
+//swich case
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=2;
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+        case 2:
+            console.log("feb");
+            break;
+             case 3:
+            console.log("march");
+            break;
+             case 4:
+            console.log("april");
+            break;
+    default:
+        console.log("not match");
+        
+        break;
+}
+
+const day="mon";
+switch (day) {
+    case "sun":
+        console.log("sunday");
+        break;
+        case "mon":
+            console.log("monday");
+            break;
+             case "tues":
+            console.log("tuesday");
+            break; 
+             case fri:
+            console.log("friday");
+            break;
+    default:
+        console.log("not match");
+        
+        break;
+}
