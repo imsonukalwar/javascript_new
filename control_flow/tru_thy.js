c0d3f4624bcffa713df0d6465ee454782cdf6dce
unknown <imsonukalwar@gmail.com>
2025-07-12 09:36:10 +0530

    new file

---
+++ control_flow/tru_thy.js
@@ -0,0 +1,47 @@
+//const useremai="sonu@kumar.ai";
+const useremai="";
+//useremail give trueth value 
+//if string is empty useremail give false vaue
+// if(useremai){
+//     console.log("got user email");
+// }
+// else{
+//     console.log("don't have user email");
+// }
+
+//falsy value 
+
+// false ,0,-0,BigInt,0n,null,undefined,NaN,""
+// 
+//truthy value
+
+// "0",'false'," ",[],{},function(){}
+
+// if(useremai.length==0){
+//     console.log("array is empty");
+// }
+
+// const m={}
+// if(Object.keys(m).length===0){
+//     console.log("object is empty");
+    
+//}
+
+
+//nulish coalescing operator (??):null undefined
+
+let val1;
+//val1= 5 ?? 10
+// val1= null ?? 10
+ val1= undefined ?? 10
+console.log(val1);
+
+
+//ternary operator
+
+// condition ? true:false
+
+const iceprise=90;
+iceprise<100?console.log("less than"):console.log("greater");
+
+
