4914cdc5765c9054574a6846d71b1d4297d36848
unknown <imsonukalwar@gmail.com>
2025-07-12 12:32:15 +0530

    new js file

---
+++ loops/05five.js
@@ -0,0 +1,36 @@
+//foreach loop
+
+
+const coding=["js","java","python","rubi"]
+
+// coding.forEach(function (val){
+//     console.log(val);}
+// )
+    
+// coding.forEach((item)=>{
+//     console.log(item);
+    
+// })
+
+// function print(item){
+//     console.log(item);
+// }
+// coding.forEach(print)
+
+// coding.forEach((item,index,arr)=>{
+// console.log(item,index,arr);
+// })
+
+const mycode=[
+    {lang:"java",
+        othlang:"python"
+    },
+    {
+        name:"sonu",
+        othname:"sanu"
+    }
+]
+mycode.forEach((item)=>{
+    console.log(item.lang);
+    
+})
