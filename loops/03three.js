5a8c6c540c87d5dd4cff91e77b28c3535f76dcc1
unknown <imsonukalwar@gmail.com>
2025-07-12 10:34:39 +0530

    new file

---
+++ loops/03three.js
@@ -0,0 +1,43 @@
+//for of
+
+// ["","",""]
+// [{},{},{}]
+
+
+const  arr=[1,22,3,10,5,6];
+for(const i of arr){
+    //console.log(i);
+    
+}
+
+// const st="hello sonu";
+// for (const i of st) {
+//     console.log(i);
+    
+// }
+
+
+//MAPS
+//it contain unique value
+
+const map=new Map();
+map.set('in',"india")
+map.set('fr',"france")
+map.set('US',"united state")
+map.set('in',"india")
+
+//console.log(map);
+for (const [i,j ] of map) {
+    console.log(i,"===>",j);
+    
+}
+
+// const m={
+//     s:"moon",
+//     o:"loot",
+//     n:"test"
+// }
+// for (const [i,j] of m) {
+//     console.log(i,j);
+    
+// }
