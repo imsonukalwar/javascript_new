de6182356147da7f1718947acdd80ee57552e12c
unknown <imsonukalwar@gmail.com>
2025-07-12 10:12:58 +0530

    new file

---
+++ loops/02two.js
@@ -0,0 +1,25 @@
+//while loop
+
+// while (condition) {
+    
+// }
+
+// let i=0;
+// while(i<5){
+//     console.log(i);
+//     i++;
+// }
+
+let array=['moon','sum','mars']
+let i=0;
+while (i<array.length) {
+    console.log(array[i]);
+    i++;
+}
+
+///do while loop
+let j=1;
+do{
+    console.log(j);
+    j++
+ } while(j<10);
