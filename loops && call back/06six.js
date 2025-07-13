a129e7aa7bf9ef6aa6ff2c6f45628ec2f50a0661
unknown <imsonukalwar@gmail.com>
2025-07-13 17:56:52 +0530

    new js file

--- loops && call back/06six.js
+++ loops && call back/06six.js
@@ -40,51 +40,51 @@ const coding=["js","java","python","rubi"]
 // console.log(newnum);
 
 const books=[
-    {title:'book one',
-    genre:'function',
-    published:1981,
-    edition:2004
-    },
+     {title:'book one',
+     genre:'function',
+     published:1981,
+     edition:2004
+     },
      {title:'book two',
-    genre:'non-function',
-    published:1992,
-    edition:2008
-    },
-         {title:'book three',
-    genre:'history',
-    published:1999,
-    edition:2007
-    },
-         {title:'book four',
-    genre:'non-function',
-    published:1989,
-    edition:2010
-    },
-         {title:'book five',
-    genre:'science',
-    published:2009,
-    edition:2014
-    },
-         {title:'book six',
-    genre:'function',
-    published:1987,
-    edition:2010
-    },
-         {title:'book seven',
-    genre:'history',
-    published:1986,
-    edition:1996
-    },
-         {title:'book eight',
-    genre:'science',
-    published:2011,
-    edition:2016
-    },
-         {title:'book nine',
-    genre:'function',
-    published:1981,
-    edition:1989
-    }
+     genre:'non-function',
+     published:1992,
+     edition:2008
+     },
+          {title:'book three',
+     genre:'history',
+     published:1999,
+     edition:2007
+     },
+          {title:'book four',
+     genre:'non-function',
+     published:1989,
+     edition:2010
+     },
+          {title:'book five',
+     genre:'science',
+     published:2009,
+     edition:2014
+     },
+          {title:'book six',
+     genre:'function',
+     published:1987,
+     edition:2010
+     },
+          {title:'book seven',
+     genre:'history',
+     published:1986,
+     edition:1996
+     },
+          {title:'book eight',
+     genre:'science',
+     published:2011,
+     edition:2016
+     },
+          {title:'book nine',
+     genre:'function',
+     published:1981,
+     edition:1989
+     }
 ]
 
 //let userbooks=books.filter((bk)=>bk.genre==='history')
