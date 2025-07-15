5b4deaeb4cc5efdd037483df4cc77237d7e5ced6
unknown <imsonukalwar@gmail.com>
2025-07-15 23:28:35 +0530

    apdate

--- dom/script.js
+++ dom/script.js
@@ -8,23 +8,7 @@ button.addEventListener('click', function() {
     paragraph.textContent = 'Text ab change ho gaya hai!';
     paragraph.style.color = 'blue';
     button.style.color="red";
-    button.innerText="clicked!";
-    button.style.backgroundColor="Yellow";
-});
-button.addEventListener('mouseover', function() {
-    // Text aur color change karna
     
-    button.style.color="red";
-    button.innerText="clicked!";
-    button.style.backgroundColor="Yellow";
-});
-
-button.addEventListener('mouseout', function() {
-    // Text aur color change karna
-    
-    button.style.color="";
-    button.innerText="Text Badlo";
-    button.style.backgroundColor="";
 });
 
 
